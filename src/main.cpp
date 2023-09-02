febff93041213d1102e130b0c8e77dc720465029
Abe Winter <abe-winter@users.noreply.github.com>
2023-09-02 16:01:39 -0400

    inherit from Generic instead of GenericService to fix crash

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
-#include <memory>
-
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/server_context.h>
-
 #include <viam/api/common/v1/common.grpc.pb.h>
 #include <viam/api/component/generic/v1/generic.grpc.pb.h>
 #include <viam/api/robot/v1/robot.pb.h>
 
+#include <iostream>
+#include <memory>
 #include <viam/sdk/components/component.hpp>
 #include <viam/sdk/components/generic/generic.hpp>
 #include <viam/sdk/config/resource.hpp>
@@ -24,54 +22,31 @@
 using viam::component::generic::v1::GenericService;
 using namespace viam::sdk;
 
-class MyModule : public GenericService::Service, public Component {
+class MyModule : public Generic {
    public:
-    API dynamic_api() const override {
-        return Generic::static_api();
-    }
+    MyModule(Dependencies deps, ResourceConfig cfg) : Generic(cfg.name()) {}
 
-    MyModule(ResourceConfig cfg) {
-        name_ = cfg.name();
-        std::cout << "Instantiating module with name " + name_ << std::endl;
-    }
+    AttributeMap do_command(AttributeMap command) { return read_wireless(); }
 
-    MyModule(const MyModule&) = delete;
-    MyModule& operator=(const MyModule&) = delete;
-
-    ::grpc::Status DoCommand(::grpc::ServerContext* context,
-                             const ::viam::common::v1::DoCommandRequest* request,
-                             ::viam::common::v1::DoCommandResponse* response) override {
-        std::cout << "docommand top" << std::endl;
-        return grpc::Status();
-        if (auto errorMsg = read_wireless(response->mutable_result())) {} else {
-            std::cout << "docommand err" << std::endl;
-            return grpc::Status(grpc::UNAVAILABLE, errorMsg);
-        }
-        std::cout << "docommand ok" << std::endl;
-        return grpc::Status();
-    }
+    std::vector<GeometryConfig> get_geometries() { return std::vector<GeometryConfig>(); }
 
    private:
     std::string name_;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc < 2) {
         throw std::invalid_argument("Pass socket path as first argument");
     }
     std::string socket_addr = argv[1];
 
-    // Use set_logger_severity_from_args to set the boost trivial logger's
-    // severity depending on commandline arguments.
     set_logger_severity_from_args(argc, argv);
 
     SignalManager signals;
 
     std::shared_ptr<ModelRegistration> mr = std::make_shared<ModelRegistration>(
-        ResourceType("MyModule"),
-        Generic::static_api(),
-        Model("acme", "demo", "printer"),
-        [](Dependencies, ResourceConfig cfg) { return std::make_unique<MyModule>(cfg); }
+        ResourceType("MyModule"), Generic::static_api(), Model("acme", "demo", "printer"),
+        [](Dependencies deps, ResourceConfig cfg) { return std::make_shared<MyModule>(deps, cfg); }
     );
 
     Registry::register_model(mr);
@@ -82,12 +57,13 @@ int main(int argc, char** argv) {
 
     my_mod->add_model_from_registry(server, mr->api(), mr->model());
     my_mod->start(server);
-    std::cout << "Module serving model " << mr->model().to_string() << ", listening on " << socket_addr << std::endl;
+    std::cout << "Module serving model " << mr->model().to_string() << ", listening on "
+              << socket_addr << std::endl;
 
     server->start();
+    // todo: can I register error handler for server?
     int sig = 0;
     auto result = signals.wait(&sig);
     server->shutdown();
-
     return 0;
 };
