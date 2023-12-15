e00a67a977c1991110e6fc93a5302c65ffb2336c
abe-winter <abe-winter@users.noreply.github.com>
2023-12-15 14:31:41 -0500

    use sensor API (#2)
    * pin to latest cpp sdk for sensor support
    
    * use sensor in place of generic
    
    * pin to latest, consume new boilerplate helpers
    
    * explicit vector param

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,25 @@
-#include <viam/sdk/components/generic/generic.hpp>
+#include <viam/sdk/components/sensor/sensor.hpp>
 #include <viam/sdk/module/service.hpp>
 
 #include "wifi.hpp"
 
 using namespace viam::sdk;
 
-class MyModule : public Generic {
+class WifiSensor : public Sensor {
    public:
-    MyModule(Dependencies deps, ResourceConfig cfg) : Generic(cfg.name()) {}
-    AttributeMap do_command(AttributeMap command) { return read_wireless(); }
-    std::vector<GeometryConfig> get_geometries() { return std::vector<GeometryConfig>(); }
+    WifiSensor(Dependencies deps, ResourceConfig cfg) : Sensor(cfg.name()) {}
+    AttributeMap do_command(const AttributeMap& command) { return {}; }
+    std::vector<GeometryConfig> get_geometries(const AttributeMap& extra) { return std::vector<GeometryConfig>(); }
+    AttributeMap get_readings(const AttributeMap& extra) { return read_wireless(); }
 };
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        throw std::invalid_argument("Pass socket path as first argument");
-    }
-    std::string socket_addr = argv[1];
-
-    set_logger_severity_from_args(argc, argv);
-
-    SignalManager signals;
-
     std::shared_ptr<ModelRegistration> mr = std::make_shared<ModelRegistration>(
-        ResourceType("MyModule"), Generic::static_api(), Model("viam", "wifi", "cpp"),
-        [](Dependencies deps, ResourceConfig cfg) { return std::make_shared<MyModule>(deps, cfg); }
+        Sensor::static_api(), Model("viam", "wifi", "cpp"),
+        [](Dependencies deps, ResourceConfig cfg) { return std::make_shared<WifiSensor>(deps, cfg); }
     );
 
-    Registry::register_model(mr);
-
-    // The `ModuleService_` must outlive the Server, so the declaration order here matters.
-    auto my_mod = std::make_shared<ModuleService_>(socket_addr);
-    auto server = std::make_shared<Server>();
-
-    my_mod->add_model_from_registry(server, mr->api(), mr->model());
-    my_mod->start(server);
-    std::cout << "Module serving model " << mr->model().to_string() << ", listening on "
-              << socket_addr << std::endl;
-
-    server->start();
-    int sig = 0;
-    auto result = signals.wait(&sig);
-    server->shutdown();
+    auto service = std::make_shared<ModuleService>(argc, argv, std::vector<std::shared_ptr<ModelRegistration>>{mr});
+    service->serve();
     return 0;
 }
