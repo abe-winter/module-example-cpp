d9ac74abdd0b5996e842c5b8f8e974258daeb1b5
Abe Winter <abe-winter@users.noreply.github.com>
2023-12-15 14:13:11 -0500

    pin to latest, consume new boilerplate helpers

--- src/main.cpp
+++ src/main.cpp
@@ -14,34 +14,13 @@ class WifiSensor : public Sensor {
 };
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        throw std::invalid_argument("Pass socket path as first argument");
-    }
-    std::string socket_addr = argv[1];
-
-    set_logger_severity_from_args(argc, argv);
-
-    SignalManager signals;
-
     std::shared_ptr<ModelRegistration> mr = std::make_shared<ModelRegistration>(
-        ResourceType("WifiSensor"), Sensor::static_api(), Model("viam", "wifi", "cpp"),
+        Sensor::static_api(), Model("viam", "wifi", "cpp"),
         [](Dependencies deps, ResourceConfig cfg) { return std::make_shared<WifiSensor>(deps, cfg); }
     );
 
-    Registry::register_model(mr);
-
-    // The `ModuleService_` must outlive the Server, so the declaration order here matters.
-    auto my_mod = std::make_shared<ModuleService_>(socket_addr);
-    auto server = std::make_shared<Server>();
-
-    my_mod->add_model_from_registry(server, mr->api(), mr->model());
-    my_mod->start(server);
-    std::cout << "Module serving model " << mr->model().to_string() << ", listening on "
-              << socket_addr << std::endl;
-
-    server->start();
-    int sig = 0;
-    auto result = signals.wait(&sig);
-    server->shutdown();
+    // The `ModuleService` must outlive the Server, so the declaration order here matters.
+    auto service = std::make_shared<ModuleService>(argc, argv, std::vector{mr});
+    service->serve();
     return 0;
 }
